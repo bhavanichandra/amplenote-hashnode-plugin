cdcfe1756d25b7bc636d483eff65b852e554f82d
bhavanichandra <bhavanichandra9@gmail.com>
2025-05-27 20:49:22 +0530

    removed external libraries and used fetch api

--- esbuild.js
+++ esbuild.js
@@ -6,7 +6,7 @@ dotenv.config();
 const result = await esbuild.build({
   entryPoints: [`lib/plugin.js`],
   bundle: true,
-  format: "esm",
+  format: "iife",
   outfile: "build/compiled.js",
   packages: "external",
   platform: "node",
